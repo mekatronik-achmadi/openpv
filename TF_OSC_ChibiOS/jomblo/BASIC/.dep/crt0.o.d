14124d13f8c7e346021051ac3099a05f24345841
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2015-05-03 06:38:29 +0700

    Initial Commit

---
+++ TF_OSC_ChibiOS/jomblo/BASIC/.dep/crt0.o.d
@@ -0,0 +1,13 @@
+build/obj/crt0.o: ../../os/ports/GCC/ARMCMx/crt0.c \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h
+
+/usr/lib/gcc/arm-none-eabi/4.8/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/features.h:
---
+++ TF_OSC_ChibiOS/jomblo/BASIC/.dep/memstreams.o.d
@@ -0,0 +1,125 @@
+build/obj/memstreams.o: ../../os/various/memstreams.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h ../../os/kernel/include/ch.h \
+ ../../boards/TF_OSC/chconf.h ../../os/ports/GCC/ARMCMx/chtypes.h \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.8/include/stdbool.h \
+ ../../os/kernel/include/chlists.h ../../os/ports/GCC/ARMCMx/chcore.h \
+ ../../os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h \
+ ../../os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../os/ports/common/ARMCMx/nvic.h ../../os/kernel/include/chsys.h \
+ ../../os/kernel/include/chvt.h ../../os/kernel/include/chschd.h \
+ ../../os/kernel/include/chsem.h ../../os/kernel/include/chbsem.h \
+ ../../os/kernel/include/chmtx.h ../../os/kernel/include/chcond.h \
+ ../../os/kernel/include/chevents.h ../../os/kernel/include/chmsg.h \
+ ../../os/kernel/include/chmboxes.h ../../os/kernel/include/chmemcore.h \
+ ../../os/kernel/include/chheap.h ../../os/kernel/include/chmempools.h \
+ ../../os/kernel/include/chthreads.h ../../os/kernel/include/chdynamic.h \
+ ../../os/kernel/include/chregistry.h ../../os/kernel/include/chinline.h \
+ ../../os/kernel/include/chqueues.h ../../os/kernel/include/chstreams.h \
+ ../../os/kernel/include/chfiles.h ../../os/kernel/include/chdebug.h \
+ ../../os/various/memstreams.h
+
+/usr/include/newlib/string.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8/include/stddef.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/sys/string.h:
+
+../../os/kernel/include/ch.h:
+
+../../boards/TF_OSC/chconf.h:
+
+../../os/ports/GCC/ARMCMx/chtypes.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8/include/stdbool.h:
+
+../../os/kernel/include/chlists.h:
+
+../../os/ports/GCC/ARMCMx/chcore.h:
+
+../../os/ports/GCC/ARMCMx/STM32F1xx/cmparams.h:
+
+../../os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../os/ports/common/ARMCMx/nvic.h:
+
+../../os/kernel/include/chsys.h:
+
+../../os/kernel/include/chvt.h:
+
+../../os/kernel/include/chschd.h:
+
+../../os/kernel/include/chsem.h:
+
+../../os/kernel/include/chbsem.h:
+
+../../os/kernel/include/chmtx.h:
+
+../../os/kernel/include/chcond.h:
+
+../../os/kernel/include/chevents.h:
+
+../../os/kernel/include/chmsg.h:
+
+../../os/kernel/include/chmboxes.h:
+
+../../os/kernel/include/chmemcore.h:
+
+../../os/kernel/include/chheap.h:
+
+../../os/kernel/include/chmempools.h:
+
+../../os/kernel/include/chthreads.h:
+
+../../os/kernel/include/chdynamic.h:
+
+../../os/kernel/include/chregistry.h:
+
+../../os/kernel/include/chinline.h:
+
+../../os/kernel/include/chqueues.h:
+
+../../os/kernel/include/chstreams.h:
+
+../../os/kernel/include/chfiles.h:
+
+../../os/kernel/include/chdebug.h:
+
+../../os/various/memstreams.h:
