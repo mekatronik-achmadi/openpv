7984fb27513e276a5d060ebf33ed641fa622aaed
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2016-04-10 15:00:35 +0700

    use system-wide build Makefile

--- rev_rb-pcb/test1-src/stdcore.mk
+++
@@ -1,9 +0,0 @@
-STDCOREASM  = $(STDLIB)/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/startup/gcc_ride7/startup_stm32f10x_md.s
-
-STDCORESRC  = $(STDLIB)/Libraries/CMSIS/CM3/CoreSupport/core_cm3.c \
-              $(STDLIB)/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c
-	     
-STDCOREINC  = $(STDLIB)/Libraries/CMSIS/CM3/CoreSupport/ \
-              $(STDLIB)/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/
-
-STDCORELD   = $(STDLIB)/Project/STM32F10x_StdPeriph_Template/TrueSTUDIO/STM32100B-EVAL/stm32_flash.ld
